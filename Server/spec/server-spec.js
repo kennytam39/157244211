ee8b3408d5cfa7986b5ae9b4cfc4099dea16c613
kennytam <kt3987@gmail.com>
2016-08-23 13:54:06 +0000

    Edit the test case, delete unuse test and add more comment

--- Server/spec/server-spec.js
+++ Server/spec/server-spec.js
@@ -3,6 +3,7 @@
 var frisby = require('frisby')
 var server = "https://assignment-kennytam.c9users.io/";
 
+//check rest api of creating user 
 frisby.create('Create a new user')
         .post(server + 'user',
                 {
@@ -10,12 +11,13 @@ frisby.create('Create a new user')
                     "password": "debugTest",
                     "email": "test@test.com",
                     "validate": false
-                }, {json: true})
+                }, {json: true})//user data
         .expectStatus(200)
         .expectJSON({"result": Boolean})
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of Email validation
 frisby.create('Email validation')
         .get(server + 'user/debugTest')
         .expectStatus(200)
@@ -23,27 +25,15 @@ frisby.create('Email validation')
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of user login function
 frisby.create('Validate login function')
-        .get(server + 'user/debugTest/debugTest')
+        .get(server + 'user/debugTest/debugTest')//this user alread create in test 1
         .expectStatus(200)
-        .expectJSON({"result": true})
+        .expectJSON({"result": Boolean})
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
-frisby.create('Validate login function with password error')
-        .get(server + 'user/debugTest/debugTest1')
-        .expectStatus(200)
-        .expectJSON({"result": false})
-        .expectHeaderContains('content-type', 'application/json')
-        .toss()
-
-frisby.create('Validate a non-exist user')
-        .get(server + 'user/testtest/test')
-        .expectStatus(200)
-        .expectJSON({"result": false})
-        .expectHeaderContains('content-type', 'application/json')
-        .toss()
-
+//check rest api of recipe searching function
 frisby.create('Get recipe list by keyword')
         .get(server + 'allRecipes/fish')
         .expectStatus(200)
@@ -51,12 +41,14 @@ frisby.create('Get recipe list by keyword')
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of recipe searching function, but using no keyword
 frisby.create('Get recipe list without keyword')
         .get(server + 'allRecipes')
         .expectStatus(404)
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of getting recipe function
 frisby.create('Get a recipe by id')
         .get(server + 'recipes/35236')
         .expectStatus(200)
@@ -66,15 +58,17 @@ frisby.create('Get a recipe by id')
                 "image_url": String,
                 "ingredients": Array,
                 "source_url": String
-            }})
+            }})//response data
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of getting recipe function, but without id
 frisby.create('Get a recipe without id')
         .get(server + 'recipes')
         .expectStatus(404)
         .toss()
 
+//check rest api of creating favourite recipe
 frisby.create('Create a favourite recipe')
         .post(server + 'favourList', {
             "recipe_id": "debugTest123",
@@ -85,12 +79,13 @@ frisby.create('Create a favourite recipe')
             "image_url": "debugTest",
             "source_url": "debugTest",
             "note": ""
-        }, {json: true})
+        }, {json: true})//favourite recipe data
         .expectStatus(200)
         .expectJSON({"result": Boolean})
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of creating favourite recipe without data
 frisby.create('Create a favourite recipe without data')
         .post(server + 'favourList')
         .expectStatus(200)
@@ -98,6 +93,7 @@ frisby.create('Create a favourite recipe without data')
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of getting all favourite recipe according to username 
 frisby.create('Get all favourite recipe list by username')
         .get(server + 'allFavourList/debugTest')
         .expectStatus(200)
@@ -105,12 +101,14 @@ frisby.create('Get all favourite recipe list by username')
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of getting all favourite recipe without username 
 frisby.create('Get all favourite recipe list without username')
         .get(server + 'allFavourList')
         .expectStatus(404)
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of specific id
 frisby.create('Get a favourite recipe by id')
         .get(server + 'favourList/debugTest123')
         .expectStatus(200)
@@ -118,12 +116,14 @@ frisby.create('Get a favourite recipe by id')
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of without id
 frisby.create('Get a favourite recipe without id')
         .get(server + 'favourList')
         .expectStatus(405)
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of updating favourite recipe
 frisby.create('Update a favourite recipe')
         .put(server + 'favourList/debugTest123', {
             "recipe_id": "debugTest123",
@@ -134,19 +134,13 @@ frisby.create('Update a favourite recipe')
             "image_url": "debugTest",
             "source_url": "debugTest",
             "note": "debugTest2"
-        }, {json: true})
+        }, {json: true})//new recipe data
         .expectStatus(200)
         .expectJSON({"result": Boolean})
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
-frisby.create('Update a favourite recipe without data')
-        .put(server + 'favourList/debugTest123')
-        .expectStatus(500)
-        .expectJSON({"result": Boolean})
-        .expectHeaderContains('content-type', 'application/json')
-        .toss()
-
+//check rest api of deleting favourite recipe
 frisby.create('Delete a favourite recipe')
         .delete(server + 'favourList/debugTest123')
         .expectStatus(200)
@@ -154,16 +148,10 @@ frisby.create('Delete a favourite recipe')
         .expectHeaderContains('content-type', 'application/json')
         .toss()
 
+//check rest api of deleting non-exist favourite recipe
 frisby.create('Delete a non-exist favourite recipe')
         .delete(server + 'favourList/debugTest123333')
         .expectStatus(200)
-        .expectJSON({"result": Boolean})
-        .expectHeaderContains('content-type', 'application/json')
-        .toss()
-
-frisby.create('Delete a favourite recipe without id')
-        .delete(server + 'favourList')
-        .expectStatus(405)
         .expectJSON({"result": Boolean})
         .expectHeaderContains('content-type', 'application/json')
         .toss()
